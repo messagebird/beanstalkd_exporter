3a4f95dd624e26e00023c697b2bbf9f6a15d7a5d
marcel corso <marcel@messagebird.com>
2019-05-29 14:53:34 +0200

    improve release of beanstalkd

--- release.sh
+++ release.sh
@@ -2,11 +2,52 @@
 # a hack to generate releases like other prometheus projects
 # use like this:
 #       VERSION=1.0.1 ./release.sh
+set -e
+
+# github user and repo
+USER=messagebird
+REPO=beanstalkd_exporter
+BIN_PACKAGE=github.com/${USER}/${REPO}
+BIN_DIR=bin
+
+if [ -z "$VERSION" ]; then
+    >&2 echo "missing VERSION=X.X.X"
+    exit 1
+fi
+
+function build_for {
+    BIN_NAME="bin/${REPO}-${VERSION}.$1-$2"
+    GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build -ldflags "-s -w" -o ${BIN_NAME} ${BIN_PACKAGE}
+    shasum -a 256 ${BIN_NAME} > ${BIN_NAME}.sha256
+}
+
+rm -rf ${BIN_DIR}
+mkdir -p ${BIN_DIR}
+
+build_for linux amd64
+build_for darwin amd64
+
+docker build -t ${USER}/${REPO}:${VERSION} .
 
 
-make release_linux
-rm -rf "bin/beanstalkd_exporter-$VERSION.linux-amd64"
-mkdir "bin/beanstalkd_exporter-$VERSION.linux-amd64"
-cp bin/beanstalkd_exporter "bin/beanstalkd_exporter-$VERSION.linux-amd64/beanstalkd_exporter"
-cd bin
-tar -zcvf "beanstalkd_exporter-$VERSION.linux-amd64.tar.gz" "beanstalkd_exporter-$VERSION.linux-amd64"
+git tag -a $VERSION -m "version $VERSION"
+
+github-release release \
+    --user $USER \
+    --repo $REPO \
+    --tag $VERSION \
+    --name $VERSION \
+    --description "version $VERSION"
+
+for release_file in $(ls ${BIN_DIR}); do
+    github-release upload \
+        --user $USER \
+        --repo $REPO \
+        --tag $VERSION \
+        --name "${release_file}" \
+        --file "${BIN_DIR}/${release_file}"
+done
+
+docker push ${USER}/${REPO}:${VERSION}
+docker tag ${USER}/${REPO}:${VERSION} ${USER}/${REPO}:latest
+docker push ${USER}/${REPO}:latest
